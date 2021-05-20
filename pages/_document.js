e4bb349aea372df6dfa27ebe96e0df6632f82eb0
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-05-19 19:05:40 -0700

    update to _document. fixed styled-components prob

--- pages/_document.js
+++ pages/_document.js
@@ -1,29 +1,60 @@
-import Document, { Html, Head, NextScript, Main } from "next/document";
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          <meta name="Grizzly Coder" content="Web development services" />
-          <link rel="icon" href="/favicon.ico" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            href="https://fonts.googleapis.com/css2?family=Orbitron:wght@600&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
+
+// import Document, { Html, Head, NextScript, Main } from "next/document";
+
+// export default class MyDocument extends Document {
+//   static async getInitialProps(ctx) {
+//     const initialProps = await Document.getInitialProps(ctx);
+//     return { ...initialProps };
+//   }
+
+//   render() {
+//     return (
+//       <Html>
+//         <Head>
+//           <meta name="Grizzly Coder" content="Web development services" />
+//           <link rel="icon" href="/favicon.ico" />
+//           <link
+//             rel="preconnect"
+//             href="https://fonts.gstatic.com"
+//             href="https://fonts.googleapis.com/css2?family=Orbitron:wght@600&display=swap"
+//             rel="stylesheet"
+//           />
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// }
