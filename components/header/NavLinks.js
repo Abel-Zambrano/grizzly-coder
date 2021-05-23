1be095e5dbb66f7f20b22d4ec5c9b0633814f9df
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-05-22 20:19:30 -0700

    added links data

--- components/header/NavLinks.js
+++ components/header/NavLinks.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Link from "next/link";
+import links from "../../data/links";
 
 const MyNavLinks = styled.div`
   width: 50rem;
---
+++ data/links.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    id: 1,
+    name: "home",
+    url: "/",
+  },
+  {
+    id: 2,
+    name: "about",
+    url: "/about",
+  },
+  {
+    id: 2,
+    name: "merchandise",
+    url: "/merchandise",
+  },
+  {
+    id: 3,
+    name: "contact",
+    url: "/contact",
+  },
+];
+
+export default links;
