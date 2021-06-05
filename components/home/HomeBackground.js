ef2eeb6c02fb68bcd128ef568165151289b4d2c3
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-06-05 11:58:12 -0700

    update to home page bg

--- components/home/HomeBackground.js
+++ components/home/HomeBackground.js
@@ -1,13 +1,61 @@
 import Image from "next/image";
+import styled from "styled-components";
+
+const Section = styled.section`
+  background-color: var(--black-light);
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: var(--black-dark);
+  height: 14rem;
+  min-width: 72rem;
+  text-align: center;
+`;
+
+const Left = styled.div`
+  background-color: red;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  .header {
+    color: var(--white);
+    text-transform: capitalize;
+    font-size: 4rem;
+  }
+
+  .text {
+    color: var(--white);
+    text-transform: capitalize;
+    font-size: 2rem;
+  }
+`;
+
+const Right = styled.div``;
 
 export default function HomeBackground() {
   return (
-    <Image
-      src=""
-      alt="background"
-      layout="responsive"
-      width="1920"
-      height="1080"
-    />
+    <Section>
+      <Left>
+        <Container>
+          <h1 className="header">connecting you to the digital world</h1>
+          <p className="text">services in web development simplified</p>
+        </Container>
+      </Left>
+      <Right>
+        <Image
+          src="/images/home-background.png"
+          alt="background"
+          layout="fixed"
+          width={500}
+          height={700}
+        />
+      </Right>
+    </Section>
   );
 }
